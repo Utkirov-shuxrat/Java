2e40aa9b41870a7de05e9a04e81a5c2c9942688a
shuxrat <shuxrat@.ru>
2022-05-13 19:03:39 +0500

    Java

---
+++ main.js
@@ -0,0 +1,17 @@
+var Prom = prompt("Введите своё имя");
+console.log("Меня зовут " + Prom);
+var age = +prompt ("Введите возрост");
+console.log("Мне " + age);
+var one = prompt("Решите пример 6 + 3=?");
+console.log("Пример 1: 6 + 3 = 9 ваш ответ " + one );
+var two = prompt("Решите пример 10 - 5 = ?");
+console.log("Пример 2 : 10 - 5 = 5 ваш ответ " + two);
+var three = prompt("Решите пример 30 * 3=?");
+console.log("Пример 3 : 30 * 3 = 90 ваш ответ " + three);
+var fore = prompt("Решите пример 80 / 2 = ?");
+console.log("Пример 4 : 80 / 2 = 40 ваш ответ " + fore);
+var five = prompt("Решите пример 13 % 2  = ?");
+console.log("Пример 5 : 13 % 2 = 1 ваш ответ " + five);
+ alert("откройте console для просмотра ")
+
+
